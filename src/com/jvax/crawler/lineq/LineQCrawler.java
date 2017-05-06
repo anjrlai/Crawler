c15a9d84d81df81f9fb159d0b8d7eac200c607ed
anjrlai <anjrlai@gmail.com>
2017-05-06 15:01:34 +0000

    Not implement classes V0.34

--- src/com/jvax/crawler/lineq/LineQCrawler.java
+++ src/com/jvax/crawler/lineq/LineQCrawler.java
@@ -1,26 +1,25 @@
 package com.jvax.crawler.lineq;
 import com.jvax.crawler.Crawler;
 import com.jvax.command.*;
+import java.util.*;
 /**
  * 網路爬蟲介面
  * 
  */
 public class LineQCrawler extends Crawler{
     
-    public void init(){
-    }
+    public void init(){}
     public String getResponse(){
         return null;
     }
-    public void parseHTML(){
-        try{
-            this.parseHTML(super.getResponse());
-        }catch(Exception e){
-            
-        }
-    }
-    public void parseHTML(String HTML){}
+    protected void parseArticle(){}
+    protected void parseArticle(String HTML){}
+    protected void parseArticleList(){};
+    protected void parseArticleList(String HTML){};
+    
     public void crawlArticle(){};
     public void crawlArticle(String Url){};
+    public Hashtable<String, String> crawlArticleList(){return null;};
+    public Hashtable<String, String> crawlArticleList(String Url){return null;};
 
 }
