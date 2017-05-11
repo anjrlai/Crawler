5fffc3b41a47a12b9e7798c92365b4e6f9709eba
anjrlai <anjrlai@gmail.com>
2017-05-11 07:15:07 +0000

    .

--- src/com/jvax/test/TestCrawler.java
+++ src/com/jvax/test/TestCrawler.java
@@ -38,32 +38,12 @@ public class TestCrawler {
         crawler.crawlArticleList(BoardUrl, ArticleCount);         /* 文章列表爬蒐（抓取網址）*/
         Urls=crawler.getUrls();
         for(String Url : Urls)
-        {
             crawler.crawlArticle(Url);      /* 單一文章爬蒐（顯示抓取內容）*/
-            crawler.showTopicReplies();
-        }
 
         /**
          * 指定輸出格式為XLS，並匯出檔案
          */
         crawler.setFormat(new XLSFormat());
         crawler.exportToFile();
-
-/***********************************************************************/
-
-        // /**
-        //  * 產製Mobile01爬蟲
-        //  */
-        // crawler = CrawlerFactory.createCrawler(CrawlerParameter.Mobile01);
-        // crawler.init();                     /* 執行初始化 */
-        // crawler.crawlArticle();             /* 文章爬蒐（抓取結果）*/
-        // // crawler.crawlArticleList();      /* 文章列表爬蒐（抓取網址）*/
-        // crawler.showTopicReplies();
-
-        // /**
-        //  * 指定輸出格式為PDF，並匯出檔案
-        //  */
-        // crawler.setFormat(new PDFFormat());
-        // crawler.exportToFile();
     };
 }
